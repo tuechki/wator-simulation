ad81d0b94c32aba8b6ec6b6e00a63bb92480f2e7
tuechki <dtuechki@gmail.com>
2023-05-17 17:24:55 +0300

    Update Shark movement and lifecycle.

--- src/bg/sofia/uni/fmi/spo/wator/Shark.java
+++ src/bg/sofia/uni/fmi/spo/wator/Shark.java
@@ -1,13 +1,36 @@
 package bg.sofia.uni.fmi.spo.wator;
 
 public class Shark extends SeaAnimal {
-    @Override
-    public void move(GridState state, Random random) {
-
+    public Shark(Position position) {
+        super(position, Constants.SHARK_INITIAL_ENERGY, Constants.SHARK_INITIAL_AGE);
     }
 
     @Override
-    public Position getFirstAvailablePos() {
+    public void move(GridState gridState) {
+
+        if (energy != Constants.MIN_ENERGY && age != Constants.SHARK_MAX_AGE) {
+            age++;
+            energy--;
+
+            if(age >= Constants.SHARK_BREEDING_AGE) {
+                gridState.addSeaAnimal(new Shark(position));
+            }
+
+            Position availablePosition = firstAvailablePosition(gridState);
+            if( availablePosition != null) {
+                energy++;
+                position = availablePosition;
+            }
+
+
+        } else {
+            gridState.removeAtPosition(position);
+        }
+    }
+
+    @Override
+    public Position firstAvailablePosition(GridState gridState) {
         return null;
     }
+
 }
