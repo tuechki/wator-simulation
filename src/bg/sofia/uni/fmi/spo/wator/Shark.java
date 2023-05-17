672be5a7cad81c32e54acb6bbcd2cb737e543cdd
tuechki <dtuechki@gmail.com>
2023-05-17 09:34:37 +0300

    Add class for the Shark that implements SeaAnimal.

--- src/bg/sofia/uni/fmi/spo/wator/Shark.java
+++ src/bg/sofia/uni/fmi/spo/wator/Shark.java
@@ -1,4 +1,13 @@
 package bg.sofia.uni.fmi.spo.wator;
 
-public class Shark {
+public class Shark extends SeaAnimal {
+    @Override
+    public void move(GridState state, Random random) {
+
+    }
+
+    @Override
+    public Position getFirstAvailablePos() {
+        return null;
+    }
 }
