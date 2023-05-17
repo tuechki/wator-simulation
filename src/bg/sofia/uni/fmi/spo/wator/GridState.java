7144c5d82d5896aeeb1065882ad471ec9419c623
tuechki <dtuechki@gmail.com>
2023-05-17 17:25:24 +0300

    Add GridState class with dummy methods.

--- src/bg/sofia/uni/fmi/spo/wator/GridState.java
+++ src/bg/sofia/uni/fmi/spo/wator/GridState.java
@@ -1,2 +1,17 @@
-package bg.sofia.uni.fmi.spo.wator;public class GridState {
+package bg.sofia.uni.fmi.spo.wator;
+
+public class GridState {
+
+    public SeaAnimal atPosition(Position position) {
+        return null;
+    }
+
+    public void removeAtPosition(Position position) {
+        //set the position to null
+    }
+
+    public void addSeaAnimal(SeaAnimal seaAnimal) {
+        //add the particular animal to the given position
+    }
+
 }
