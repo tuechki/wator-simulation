2d02062a2a113c1aa86d478373eba387a49f6209
tuechki <dtuechki@gmail.com>
2023-05-17 09:08:39 +0300

    Add methods to get all direction positions.

--- src/bg/sofia/uni/fmi/spo/wator/SeaAnimal.java
+++ src/bg/sofia/uni/fmi/spo/wator/SeaAnimal.java
@@ -1,4 +1,37 @@
 package bg.sofia.uni.fmi.spo.wator;
 
-public abstract class Animal {
+public abstract class SeaAnimal {
+    protected Position position;
+    protected int energy;
+    protected int age;
+
+    public SeaAnimal(Position position, int energy, int age) {
+        this.position = position;
+        this.energy = energy;
+        this.age = age;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Position getNorthPosition() {
+        return new Position(getPosition().row() + 1, getPosition().col());
+    }
+
+    public Position getSouthPosition() {
+        return new Position(getPosition().row() - 1, getPosition().col());
+    }
+
+    public Position getEastPosition() {
+        return new Position(getPosition().row(), getPosition().col() + 1);
+    }
+
+    public Position getWestPosition() {
+        return new Position(getPosition().row(), getPosition().col() - 1);
+    }
 }
