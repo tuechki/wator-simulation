033e5781adf97c7a0530d8f6123e43d02d083a30
tuechki <dtuechki@gmail.com>
2023-05-17 17:23:55 +0300

    Add constants for sharks and fish.

---
+++ src/bg/sofia/uni/fmi/spo/wator/Constants.java
@@ -0,0 +1,20 @@
+package bg.sofia.uni.fmi.spo.wator;
+
+public class Constants {
+
+
+
+    public static final int ENERGY_IMMORTAL = -1;
+    public static final int FISH_INITIAL_ENERGY = ENERGY_IMMORTAL;
+    public static final int FISH_INITIAL_AGE = 0;
+    public static final int FISH_MAX_AGE = 10;
+    public static final int FISH_BREEDING_AGE = 3;
+
+
+    public static final int SHARK_INITIAL_ENERGY = 10;
+    public static final int SHARK_INITIAL_AGE = 0;
+    public static final int SHARK_MAX_AGE = 30;
+    public static final int SHARK_BREEDING_AGE = 15;
+
+    public static final int MIN_ENERGY = 0;
+}
