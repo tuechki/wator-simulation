bb4a9112a854c04e98d26b1ecfcfde802e1fea61
tuechki <dtuechki@gmail.com>
2023-05-26 10:23:42 +0300

    Move logic for neighbours to GridState. Add method to extract all neighbouring sea animals at a position.

--- src/bg/sofia/uni/fmi/spo/wator/GridState.java
+++ src/bg/sofia/uni/fmi/spo/wator/GridState.java
@@ -1,5 +1,8 @@
 package bg.sofia.uni.fmi.spo.wator;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GridState {
 
     public SeaAnimal atPosition(Position position) {
@@ -14,4 +17,31 @@ public class GridState {
         //add the particular animal to the given position
     }
 
+    public Map<Position, SeaAnimal> getNeighboursAtPosition(Position position) {
+
+        Map<Position, SeaAnimal> neighbouringSeaAnimals = new HashMap();
+        neighbouringSeaAnimals.put(northOf(position), atPosition(northOf(position)));
+        neighbouringSeaAnimals.put(eastOf(position), atPosition(eastOf(position)));
+        neighbouringSeaAnimals.put(southOf(position), atPosition(southOf(position)));
+        neighbouringSeaAnimals.put(westOf(position), atPosition(westOf(position)));
+
+        return neighbouringSeaAnimals;
+    }
+
+
+    public Position northOf(Position position) {
+        return new Position(position.row() + 1, position.col());
+    }
+
+    public Position southOf(Position position) {
+        return new Position(position.row() - 1, position.col());
+    }
+
+    public Position eastOf(Position position) {
+        return new Position(position.row(), position.col() + 1);
+    }
+
+    public Position westOf(Position position) {
+        return new Position(position.row(), position.col() - 1);
+    }
 }
