baec89cf695923fc406053ba5cc9cbdf6ae439e3
tuechki <dtuechki@gmail.com>
2023-05-26 12:13:26 +0300

    Add static intialRandom method to generate grid and implements methods.

--- src/bg/sofia/uni/fmi/spo/wator/GridState.java
+++ src/bg/sofia/uni/fmi/spo/wator/GridState.java
@@ -1,23 +1,71 @@
 package bg.sofia.uni.fmi.spo.wator;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 
 public class GridState {
 
+    public static GridState initialRandom(int height, int width, int fishCount, int sharkCount) {
+
+        var random = new Random(0);
+        GridState gridState = new GridState(height, width, new HashSet<>());
+
+        int currentFishCount = 0;
+        while (currentFishCount < fishCount) {
+            var randomPosition = new Position(random.nextInt(height), random.nextInt(width));
+
+            if(gridState.isEmptyPosition(randomPosition)) {
+                gridState.addSeaAnimal(new Fish(randomPosition));
+                currentFishCount++;
+            }
+        }
+
+        int currentSharkCount = 0;
+        while (currentSharkCount < sharkCount) {
+            var randomPosition = new Position(random.nextInt(height), random.nextInt(width));
+
+            if(gridState.isEmptyPosition(randomPosition)) {
+                gridState.addSeaAnimal(new Shark(randomPosition));
+                currentSharkCount++;
+            }
+        }
+
+        return gridState;
+    }
+
+    private Set<SeaAnimal> seaAnimals;
+    private final int height;
+    private final int width;
+
+    private GridState(int height, int width, Set<SeaAnimal> seaAnimals) {
+        this.height = height;
+        this.width = width;
+        this.seaAnimals = seaAnimals;
+    }
+
     public SeaAnimal atPosition(Position position) {
-        return null;
+        return seaAnimals().stream()
+            .filter(seaAnimal -> seaAnimal.position().equals(position))
+            .findAny()
+            .orElse(null);
+    }
+
+    public boolean isEmptyPosition(Position position) {
+        return atPosition(position) == null;
     }
 
     public void removeAtPosition(Position position) {
-        //set the position to null
+        seaAnimals.remove(SeaAnimal.of(position));
     }
 
     public void addSeaAnimal(SeaAnimal seaAnimal) {
-        //add the particular animal to the given position
+        seaAnimals.add(seaAnimal);
     }
 
-    public Map<Position, SeaAnimal> getNeighboursAtPosition(Position position) {
+    public Map<Position, SeaAnimal> neighboursAtPosition(Position position) {
 
         Map<Position, SeaAnimal> neighbouringSeaAnimals = new HashMap<>();
         neighbouringSeaAnimals.put(northOf(position), atPosition(northOf(position)));
@@ -27,7 +75,6 @@ public class GridState {
 
         return neighbouringSeaAnimals;
     }
-
 
     public Position northOf(Position position) {
         return new Position(position.row() + 1, position.col());
@@ -43,5 +90,17 @@ public class GridState {
 
     public Position westOf(Position position) {
         return new Position(position.row(), position.col() - 1);
+    }
+
+    public Set<SeaAnimal> seaAnimals() {
+        return seaAnimals;
+    }
+
+    public int height() {
+        return height;
+    }
+
+    public int width() {
+        return width;
     }
 }
