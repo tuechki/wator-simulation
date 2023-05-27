2f572b7c7f6487d44e110c1dababb184b5899840
tuechki <dtuechki@gmail.com>
2023-05-27 10:41:06 +0300

    Update constants.

--- src/bg/sofia/uni/fmi/spo/wator/Constants.java
+++ src/bg/sofia/uni/fmi/spo/wator/Constants.java
@@ -1,5 +1,7 @@
 package bg.sofia.uni.fmi.spo.wator;
 
+import java.awt.*;
+
 public class Constants {
 
 
@@ -17,4 +19,20 @@ public class Constants {
     public static final int SHARK_BREEDING_AGE = 15;
 
     public static final int MIN_ENERGY = 0;
+
+
+    public static final int WORLD_HEIGHT = 1080;
+    public static final int WORLD_WIDTH = 1920;
+    public static final int FISH_COUNT = 1000000;
+    public static final int SHARKS_COUNT = 10000;
+    public static final int THREADS_COUNT = 8;
+    public static final int ITERATIONS = 100;
+
+
+
+    public static final int FRAME_INTERVAL_MILLIS = 40;
+    public static final int CELL_SIZE = 1;
+    public static final Color OCEAN_COLOR = new Color(33, 33, 33);
+    public static final Color FISH_COLOR = new Color(102, 187, 106);
+    public static final Color SHARK_COLOR = new Color(33, 150, 243);
 }
