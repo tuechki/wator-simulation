9327e54de13edb821de399381b506d727dd944dd
tuechki <dtuechki@gmail.com>
2023-06-09 02:22:51 +0300

    Add enum to descripe SeaAnimal types.

---
+++ src/bg/sofia/uni/fmi/spo/wator/SeaAnimalType.java
@@ -0,0 +1,12 @@
+package bg.sofia.uni.fmi.spo.wator;
+
+public enum SeaAnimalType {
+    FISH("Fish"),
+    SHARK("Shark");
+
+    public final String type;
+
+    SeaAnimalType(String type) {
+        this.type = type;
+    }
+}
